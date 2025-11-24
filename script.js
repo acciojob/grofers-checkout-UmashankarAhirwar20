506cf46135dd84d310c94ca0ce330763921e9253
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2025-11-24 16:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const priceOfItems = document.querySelectorAll(".price");
+	
+	let totalValue = 0;
+	priceOfItems.forEach(i => {
+		totalValue += Number(i.textContent);
+	});
+
+	const table = document.querySelector("table");
+    const newRow = document.createElement("tr");
+    const totalCell = document.createElement("td");
+
+    totalCell.colSpan = 2;
+    totalCell.textContent = `Total Price = Rs ${totalValue}`;
+
+    newRow.appendChild(totalCell);
+    table.appendChild(newRow);
   
 };
 
